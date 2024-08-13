09906a5c85759e4a3d594987cdd5e112c0da0a75
Victor Jouffrey <vicatjou@gmail.com>
2024-08-13 12:47:11 +0200

    Add some activity rich print

--- python/hopsworks_common/feature_store_activity.py
+++ python/hopsworks_common/feature_store_activity.py
@@ -25,9 +25,9 @@ import humps
 from hopsworks import execution as execution_mod
 from hopsworks_common import user as user_mod
 from hopsworks_common import util
+from hopsworks_common.core.constants import HAS_GREAT_EXPECTATIONS, HAS_RICH
 from hsfs import expectation_suite as es_mod
 from hsfs import validation_report as vr_mod
-from hsfs.core.constants import HAS_GREAT_EXPECTATIONS
 
 
 if TYPE_CHECKING or HAS_GREAT_EXPECTATIONS:
@@ -35,6 +35,17 @@ if TYPE_CHECKING or HAS_GREAT_EXPECTATIONS:
         ExpectationSuite,
         ExpectationSuiteValidationResult,
     )
+
+if HAS_RICH:
+    from rich import box
+    from rich.markdown import Markdown
+    from rich.table import Table
+
+    ACTIVITY_STATISTICS_MARKDOWN = (
+        """Percentage of data: {row_percentage}%,\n{window_start} to {window_end}."""
+    )
+    COMMIT_STATISTICS_MARKDOWN = """Ingested {rows_ingested} rows:\n- inserted: {percentage_rows_inserted:.0f}%\n- updated: {percentage_rows_updated:.0f}%\n- deleted: {percentage_rows_deleted:.0f}%"""
+    EMPTY_COMMIT_MARKDOWN = "Ingestion Job was triggered but found no ingested data."
 
 
 class FeatureStoreActivityType(Enum):
@@ -155,12 +166,9 @@ class FeatureStoreActivity:
         return json.dumps(self.to_dict(), cls=util.Encoder)
 
     def __repr__(self):
-        utc_human_readable = (
-            datetime.datetime.fromtimestamp(
-                self.timestamp / 1000, datetime.timezone.utc
-            ).strftime(r"%Y-%m-%d %H:%M:%S")
-            + " UTC"
-        )
+        utc_human_readable = datetime.datetime.fromtimestamp(
+            self.timestamp / 1000, datetime.timezone.utc
+        ).strftime(r"%Y-%m-%d %H:%M:%S UTC")
         the_string = f"Activity {self.type.value},"
         the_string += f" at: {utc_human_readable}"
         if self.user:
@@ -168,7 +176,11 @@ class FeatureStoreActivity:
         if self.metadata:
             the_string += f"\n\t{self.metadata}"
         if self.execution:
-            the_string += f"\n{self.execution.get_url()},"
+            if self.execution.execution_final_status:
+                the_string += f"\n{self.execution.final_status} at {self.execution_last_event_time}"
+            the_string += (
+                f"\nCheck it in the Hopsworks UI : {self.execution.get_url()},"
+            )
         if self.validation_report:
             the_string += f"Validation {'succeeded' if self.validation_report.success else 'failed'}."
         if self.expectation_suite:
@@ -181,3 +193,102 @@ class FeatureStoreActivity:
             the_string += f"\nData ingestion:\n{json.dumps(self.commit, indent=2)}"
 
         return the_string
+
+    def _to_rich_row(self) -> List[Any]:
+        row_entries = [
+            self.type.value,
+        ]
+        row_entries.append(
+            datetime.datetime.fromtimestamp(
+                self.timestamp / 1000, datetime.timezone.utc
+            ).strftime(r"%Y-%m-%d %H:%M:%SUTC")
+        )
+        if self.type == FeatureStoreActivityType.METADATA:
+            row_entries.append(self.metadata)
+        if self.execution:
+            row_entries.append(
+                Markdown(f"[Check it here ]({self.execution.get_url()})")
+            )
+        if self.validation_report:
+            row_entries.append(
+                f"Validation {'Succeeded' if self.validation_report.success else 'Failed'}"
+            )
+        if self.expectation_suite:
+            row_entries.append(
+                f"Updated : {len(self.expectation_suite.expectations)} expectations"
+            )
+        if self.statistics:
+            row_entries.append(build_statistics_markdown(self.statistics))
+        if self.commit:
+            row_entries.append(build_commit_markdown(self.commit))
+
+        row_entries.append(self.user.email if self.user else "")
+        return row_entries
+
+
+if HAS_RICH:
+
+    def build_activity_table(activities: List[FeatureStoreActivity]):
+        activities = sorted(activities, key=lambda x: x.timestamp, reverse=True)
+        table = Table(
+            show_header=True,
+            header_style="bold",
+            box=box.ASCII2,
+            title="Activity Timeline",
+        )
+        table.add_column("Type")
+        table.add_column("Timestamp")
+        table.add_column("Metadata")
+        table.add_column("User")
+
+        for activity in activities:
+            table.add_row(*activity._to_rich_row())
+
+        return table
+
+    def build_statistics_markdown(
+        statistics: Dict[str, Union[str, int, float]],
+    ) -> Markdown:
+        window_start = statistics.get("window_start_commit_time", 0)
+        if window_start == 0:
+            window_start_str = "ingested up"
+        else:
+            window_start_str = datetime.datetime.fromtimestamp(
+                window_start / 1000,
+                datetime.timezone.utc,
+            ).strftime(r"%Y-%m-%d %H:%M:%SUTC")
+        return Markdown(
+            ACTIVITY_STATISTICS_MARKDOWN.format(
+                window_start=window_start_str,
+                window_end=datetime.datetime.fromtimestamp(
+                    statistics.get("window_end_commit_time") / 1000,
+                    datetime.timezone.utc,
+                ).strftime(r"%Y-%m-%d %H:%M:%SUTC"),
+                row_percentage=statistics.get("row_percentage", 0) * 100,
+            )
+        )
+
+    def build_commit_markdown(
+        commit: Dict[str, Union[str, int, float]],
+    ) -> Markdown:
+        rows_ingested = (
+            commit.get("rows_inserted", 0)
+            + commit.get("rows_updated", 0)
+            + commit.get("rows_deleted", 0)
+        )
+        if rows_ingested == 0:
+            return Markdown(EMPTY_COMMIT_MARKDOWN)
+        return Markdown(
+            COMMIT_STATISTICS_MARKDOWN.format(
+                rows_ingested=rows_ingested,
+                percentage_rows_inserted=commit.get("rows_inserted", 0)
+                / rows_ingested
+                * 100,
+                percentage_rows_updated=commit.get("rows_updated", 0)
+                / rows_ingested
+                * 100,
+                percentage_rows_deleted=commit.get("rows_deleted", 0)
+                / rows_ingested
+                * 100,
+            )
+        )
